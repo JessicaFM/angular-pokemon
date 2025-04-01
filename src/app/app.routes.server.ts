bfbd65d84969650ae4193146fe6565f04f46486c
Jessica F Martinez <jessicafndez@gmail.com>
2025-04-01 20:27:58 +0200

    some updates

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -2,7 +2,15 @@ import { RenderMode, ServerRoute } from '@angular/ssr';
 
 export const serverRoutes: ServerRoute[] = [
   {
-    path: '**',
+    path: '', // solo la home
     renderMode: RenderMode.Prerender
+  },
+  {
+    path: 'details/:id',
+    renderMode: RenderMode.Server
+  },
+  {
+    path: '**',
+    renderMode: RenderMode.Client
   }
 ];
